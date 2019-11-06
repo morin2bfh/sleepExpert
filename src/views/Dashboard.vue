235512ad51a6ad292c21f8fd4b87cb900f93ce44
Jeannine <jeannine_siehen@hotmail.com>
2019-11-06 12:00:52 +0100

    weitergabe von dashboard an morin2

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,3 +1,37 @@
 <template>
-    <h1>Dashboard</h1>
+    <v-app>
+    <v-content>
+        <h1>Dashboard</h1>
+      <router-view></router-view>
+      <Card> </Card>
+    </v-content>
+    </v-app>
 </template>
+<script>
+export default {
+     data() {
+    return {
+      routes: [
+        {
+          name: "Dashboard",
+          icon: "$vuetify.icons.dashboard",
+          route: "/dashboard"
+        },
+        {
+          name: "Morgeneintrag",
+          icon: "$vuetify.icons.morning",
+          route: "/entryMorning"
+        },
+        {
+          name: "Abendeintrag",
+          icon: "$vuetify.icons.evening",
+          route: "/entryEvening"
+        },
+        { name: "Verlauf", icon: "$vuetify.icons.history", route: "/history" },
+        { name: "Statistik", icon: "$vuetify.icons.stats", route: "/stats" },
+        { name: "Info", icon: "$vuetify.icons.info", route: "/info" }
+      ],
+    }
+    }
+};
+</script>
