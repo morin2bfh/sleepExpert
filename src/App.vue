3d9a228fa900e449f985019fbffaae04bb3e2c5d
morin2bfh <morin2@bfh.ch>
2019-11-06 10:10:50 +0100

    nevermind...This commit is about the icons. AGAIN!

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,13 @@
     <v-content>
       <router-view></router-view>
     </v-content>
-    <v-bottom-navigation v-model="activeNav" grow>
-      <v-btn class="pt-5" :value=route.name v-for="route in routes" :key="route.name" router :to="route.route" >
+    <v-bottom-navigation v-model="activeNav" grow horizontal dark>
+      <v-btn
+        :value="route.name"
+        v-for="route in routes"
+        :key="route.name"
+        @click="goTo(route.route)"
+      >
         <span class="overline">{{route.name}}</span>
         <v-icon>{{route.icon}}</v-icon>
       </v-btn>
@@ -14,22 +19,37 @@
 
 <script>
 export default {
-  name: 'App',
-  components: {
-  },
-  data (){
-    return{
+  name: "App",
+  components: {},
+  data() {
+    return {
       routes: [
-        {name: "Dashboard", icon: '$vuetify.icons.dashboard', route: "/dashboard"},
-        {name: "Morgeneintrag", icon: '$vuetify.icons.morning', route:"/entryMorning"},
-        {name: "Abendeintrag", icon: '$vuetify.icons.evening', route:"/entryEvening"},
-        {name: "Verlauf", icon: '$vuetify.icons.history', route:"/history"},
-        {name: "Statistik", icon: '$vuetify.icons.stats', route:"/stats"},
-        {name: "Info", icon: '$vuetify.icons.info', route:"/info"},
+        {
+          name: "Dashboard",
+          icon: "$vuetify.icons.dashboard",
+          route: "/dashboard"
+        },
+        {
+          name: "Morgeneintrag",
+          icon: "$vuetify.icons.morning",
+          route: "/entryMorning"
+        },
+        {
+          name: "Abendeintrag",
+          icon: "$vuetify.icons.evening",
+          route: "/entryEvening"
+        },
+        { name: "Verlauf", icon: "$vuetify.icons.history", route: "/history" },
+        { name: "Statistik", icon: "$vuetify.icons.stats", route: "/stats" },
+        { name: "Info", icon: "$vuetify.icons.info", route: "/info" }
       ],
-      activeNav: '',
-
-    }
+      activeNav: ""
+    };
   },
+  methods: {
+    goTo(route) {
+      this.$router.push(route);
+    }
+  }
 };
 </script>
