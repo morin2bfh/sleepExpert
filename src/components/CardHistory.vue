f43026ccc33b4b319e2dd86183ded5abbd735da6
jeannine <burkj7@bfh.ch>
2019-12-06 15:20:18 +0100

    push Branch

---
+++ src/components/CardHistory.vue
@@ -0,0 +1,31 @@
+<template>
+   
+   <v-card dark @click="goTo(route)" class="mx-auto mt-2" max-width="400">
+      <v-img 
+        class="white--text align-end" 
+        contain height="15vh" 
+        v-if="option == 'optionEntry'"
+        :src="imgSrc">
+      </v-img>
+    <v-card-title>{{ title }}</v-card-title>
+   </v-card>
+</template>
+<script>
+
+export default {
+  name: "card",
+  props: {
+    title: String,
+    imgSrc: String,
+    option: String,
+    route: String
+  },
+  methods: {
+    goTo(route) {
+     this.$router.push(route);
+    }
+  }
+};
+</script>
+<style scoped>
+</style>
--- src/views/History.vue
+++ src/views/History.vue
@@ -1,4 +1,40 @@
 <template>
-  <div>
-  </div>
+ <v-container id="containerHistory">
+  <card-history
+      v-for="card in cards"
+      :key="card.title"
+      :imgSrc="card.imgSrc"
+      :title="card.title"
+      :option="card.option"
+      :route="card.route"
+    />
+ </v-container>
 </template>
+
+<script>
+import CardHistory from '../components/CardHistory.vue'
+
+export default {
+  components:{
+    CardHistory,
+  },
+   data() {
+    return {
+      cards: [
+        {
+          imgSrc: require("../assets/sun.png"),
+          option: "morningEntry",
+          title: "getDateFromDBMorning",
+          route: "/entryMorning"
+        },
+        {
+          imgSrc: require("../assets/moon.png"),
+          option: "eveningEntry",
+          title: "getDateFromDBEvening",
+          route: "/entryEvening"
+        },
+      ]
+    };
+   }
+};  
+</script>
