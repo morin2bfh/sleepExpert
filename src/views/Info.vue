6afd88426e39c856c8159c2c63bb04eea9fc972a
jeannine <burkj7@bfh.ch>
2019-12-09 16:32:41 +0100

    Created Information view analog dashboard view

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -1,4 +1,49 @@
 <template>
-  <div>
-  </div>
+  <v-container class="d-flex flex-column justify-space-around" id="container">
+    <card
+      v-for="card in cards"
+      :key="card.title"
+      :title="card.title"
+      :subtitle="card.subtitle"
+      :imgSrc="card.imgSrc"
+      :route="card.route"
+    />
+  </v-container>
 </template>
+<script>
+import Card from "@/components/Card.vue";
+//import Router from "../router.js";
+
+export default {
+  components: {
+    Card,
+  },
+  data: () => ({
+    cards: [
+      {
+        title: "Übungen",
+        subtitle: "Übungen rund um das Thema Schlaf",
+        imgSrc: require("../assets/meditation.png"),
+        route: "/", 
+      },
+      {
+        title: "Wissen",
+        subtitle:"Wissen rund um das Thema Schlaf",
+        imgSrc: require("../assets/questionmark.png"),
+        route: "/"
+      },
+      {
+        title: "Tipps",
+        subtitle: "Tipps rund ums Thema Schlaf",
+        imgSrc: require("../assets/lamp.png"),
+        route: "/"
+      }
+    ]
+  })
+};
+</script>
+<style scoped>
+#container {
+  height: calc(100% - 100px);
+}
+</style>
