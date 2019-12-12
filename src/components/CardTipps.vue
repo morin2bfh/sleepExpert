359b205e6d26726cc60be4a09946064d2321f515
jeannine <burkj7@bfh.ch>
2019-12-12 14:16:40 +0100

    Finished Tipps.vue site

--- src/components/CardTipps.vue
+++ src/components/CardTipps.vue
@@ -1,16 +1,16 @@
  <template>
-    <v-card dark class="mx-auto mt-2">
-        <v-card-title>{{title}}</v-card-title>
-        <v-card-actions justify="end">
-            <v-btn icon @click="show = !show">
-                <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+    <v-card @click="show = !show" dark class="mx-auto mt-2" id="styleCard">
+        <v-card-title bold>{{title}}</v-card-title>
+        <v-card-actions>
+          <v-expand-transition>
+            <div v-show="show">
+                <v-card-text id="styleText" bold>{{textExpanded}}</v-card-text>
+            </div>
+          </v-expand-transition>
+          <v-btn icon>
+            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
             </v-btn>
         </v-card-actions>
-        <v-expand-transition>
-            <div v-show="show">
-                <v-card-text>{{textExpanded}}</v-card-text>
-            </div>
-        </v-expand-transition>
     </v-card>
   </template>
    
@@ -30,5 +30,18 @@
   <style>
   .v-card__actions {
     justify-content: flex-end;
+}
+.v-card__title {
+  word-break: break-word;
+  padding: 4px;
+  }
+  .v-card__actions {
+    padding: 0px;
+  }
+#styleText{
+ font-size: 1.2rem;
+}
+#styleCard{
+  padding: 10px;
 }
   </style>
--- src/views/Tipps.vue
+++ src/views/Tipps.vue
@@ -28,7 +28,7 @@ export default {
       },
       {
         title: "Regelmässige körperliche Aktivität",
-        textExpanded: "Koffein zählt als 'Wachmacher', was ungeeignet ist, um einzuschlafen.",
+        textExpanded: "Der Körper muss ermüden, damit er schlafen kann. Aber Sport kurz vor dem Schlafen aktiviert einige Menschen, daher eher früher planen.",
       },
       {
         title: "Tagesschlaf versuchen zu reduzieren",
@@ -36,19 +36,19 @@ export default {
       },
       {
         title: "Nicht auf die Uhr/Wecker schauen in der Nacht",
-        textExpanded: "Koffein zählt als 'Wachmacher', was ungeeignet ist, um einzuschlafen.",
+        textExpanded: "Kann dazu führen, dass man sich noch mehr unter Druck setzt (z.B.: Jetzt ist es schon 4 Uhr, jetzt hab ich nur noch 2 Stunden…')",
       },
       {
         title: "Morgendliche Lichtexposition",
-        textExpanded: "Koffein zählt als 'Wachmacher', was ungeeignet ist, um einzuschlafen.",
+        textExpanded: "Wichtigster Zeitgeber für unseren Körper, setzt unseren Biorhythmus. Z.B.: Tageslichtlampen",
       },
       {
         title: "Licht abends reduzieren",
-        textExpanded: "Koffein zählt als 'Wachmacher', was ungeeignet ist, um einzuschlafen.",
+        textExpanded: "Blaulichtfilter auf dem Smartphone nutzen.",
       },
       {
-        title: "Gestaltung der gewonnenen Zeit",
-        textExpanded: "Koffein zählt als 'Wachmacher', was ungeeignet ist, um einzuschlafen.",
+        title: "Schlafumgebung günstig gestalten",
+        textExpanded: "Z.B.: Dunkel, ruhig, angenehme Temperatur",
       },
     ]
   })
@@ -56,6 +56,7 @@ export default {
 </script>
 <style scoped>
 #container {
-  height: calc(100% - 100px);
+  height: calc(100% - 150px);
+  padding-bottom: 80px;
 }
 </style>
