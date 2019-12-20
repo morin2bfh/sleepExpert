177cbb74761386d8b834355c87401d1a35d8f23a
morin2bfh <morin2@bfh.ch>
2019-12-20 23:53:49 +0100

    get history from broken branch

--- src/views/History.vue
+++ src/views/History.vue
@@ -1,48 +1,77 @@
 <template>
   <v-container class="flex-column" id="container">
-  <card-history
+    <card-history
       v-for="card in cards"
-      :key="card.title"
-      :imgSrc="card.imgSrc"
-      :title="card.title"
-      :route="card.route"
+      :key="card.data().timestamp.seconds"
+      :imgSrc="getImgSrc(card.data())"
+      :title="format(card.data().timestamp.seconds)"
+      :route="card.id"
+      :data="card.data()"
+      :morning="isMorning(card.data())"
     />
- </v-container>
+  </v-container>
 </template>
 
 <script>
-import CardHistory from '../components/CardHistory.vue'
+import CardHistory from "../components/CardHistory.vue";
+import db from "../fb";
+import { auth } from "../fb";
 
 export default {
-  components:{
-    CardHistory,
+  components: {
+    CardHistory
   },
-   data() {
+  data() {
     return {
-      cards: [
-        {
-          imgSrc: require("../assets/sun.png"),
-          title: "12. Dezember 2019",
-          route: "/entryMorning",
-        },
-        {
-          imgSrc: require("../assets/moon.png"),
-          title: "12. Dezember 2019",
-          route: "/historyEveningEn",
-        },{
-          imgSrc: require("../assets/sun.png"),
-          title: "11. Dezember 2019",
-          route: "/entryMorning",
-        },
-        {
-          imgSrc: require("../assets/moon.png"),
-          title: "11.12.2019",
-          route: "/entryEvening",
-        },
-      ]
+      cards: []
     };
-   }
-};  
+  },
+  created() {
+    var entries = [];
+
+    var p1 = db
+      .collection("EntryMorning")
+      .where("uid", "==", auth.currentUser.uid)
+      .get();
+
+    var p2 = db
+      .collection("EntryEvening")
+      .where("uid", "==", auth.currentUser.uid)
+      .get();
+
+    Promise.all([p1, p2]).then(values => {
+      var resultMorn = values[0];
+      resultMorn.forEach(function(doc) {
+        entries.push(doc);
+      });
+      var resultEv = values[1];
+      resultEv.forEach(function(doc) {
+        entries.push(doc);
+      });
+      entries.sort((a, b) => {
+        return -1 * (a.data().timestamp.seconds - b.data().timestamp.seconds);
+      });
+      this.cards = entries;
+    });
+  },
+  methods: {
+    format(unix) {
+      return new Date(unix * 1000).toLocaleDateString("de-DE");
+    },
+    getImgSrc(entry) {
+      var src = "";
+      if (entry.bedTime) {
+        src = require("../assets/sun.png");
+      } else if (entry.concentration) {
+        src = require("../assets/moon.png");
+      }
+      return src;
+    },
+    isMorning(entry) {
+      return entry.bedTime ? true : false;
+    }
+  }
+};
 </script>
 <style scoped>
 </style>
