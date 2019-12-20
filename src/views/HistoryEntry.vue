fae72d38193a8cf61db7ceeb26b4e9ddb08897f5
morin2bfh <morin2@bfh.ch>
2019-12-21 00:00:09 +0100

    get file from broken branch

---
+++ src/views/HistoryEntry.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hello</h1>
+</template>
+<script>
+export default {
+  created() {
+    console.log(this.$route.params.id);
+    console.log(this.$route.params.data);
+  }
+};
+</script>
+<style scoped>
+</style>
